9ee66a9f96b149b79543b145a819203fb23a627f
G1gg1L3s <43041209+G1gg1L3s@users.noreply.github.com>
2021-02-15 22:30:34 +0200

    Review code.cpp

--- lab1.1/code.cpp
+++ lab1.1/code.cpp
@@ -3,12 +3,15 @@
 #include <omp.h>
 #include <random>
 
-int convertToInt(char* a, int size) { 
+// It's better to just use std::stoi
+int convertToInt(char* a, int size) {
+    // Always init your variables. For this occasion it doesn't matter but it will save you a lot of time 
     int i; 
     std::string s = ""; 
     for (i = 0; i < size; i++) { 
         s = s + a[i]; 
     } 
+    // C-style cast. Use static_cast<> 
     return (int) std::stoi(s); 
 } 
 
@@ -16,13 +19,14 @@ struct Point {
     
     double x;
     double y;
-
+    // There is no need for this constructor. Compiler automatically generates it
     Point (float xCoor = 0, float yCoor = 0) {
         x = xCoor;
         y = yCoor;
     }
 };
 
+// Inconsistent formatting ") {"
 int main(int argc, char** argv){
 
     // init variables
@@ -42,9 +46,15 @@ int main(int argc, char** argv){
     // Init array of Point's
     const float T = 0.15;  // Time of signal in seconds
     const double stepSize = 0.000005;
+    // Inconsistent formatting "T / stepSize"
     const int amountOfPoints = T/stepSize;
+    
+    // For this case it doesn't matter but it's good practice to init your arrays. 
+    // It can save you from some debugging and if you care about speed, compilers can easily optimise initialization
+    // Just put "Point xt [amountOfPoints]{};" or "Point xt [amountOfPoints] = {};"
     Point xt [amountOfPoints];
-
+    
+    // Inconsistent formatting "W / n" and "for( "
     // Iterate through the several harmonics
     for( int w = W/n; w <= W; w += W/n) {
         double A = amp_and_fi(generator);
@@ -60,6 +70,8 @@ int main(int argc, char** argv){
     }   
 
     // Calculating mathematical expectation
+    // You can use standart algorithms for it, but it's ok
+    // initialize sumOfPoints
     double sumOfPoints;
     for(int i = 0; i < amountOfPoints; i++) {
         sumOfPoints += xt[i].y;
@@ -79,6 +91,7 @@ int main(int argc, char** argv){
     // Write calculations to file
     std::ofstream dataSheet;
     dataSheet.open ("data.xlsx");
+    // it's good practice to check if file is opened because of permissions and other stuff. 
     for(int i = 0; i < amountOfPoints; i++) {
         dataSheet << xt[i].x << "\t" << xt[i].y << '\n';
     }
